34eed877dd6dcc619d03eb94952f135b1e53b35d
willing <willing@supervlabs.io>
2024-09-02 14:59:42 +0900

    updated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ts-project
+
+
+```bash
+pnpm i @metaplex-foundation/umi
+pnpm i @metaplex-foundation/umi-bundle-defaults
+pnpm i @solana/web3.js
+pnpm i @metaplex-foundation/umi --save-peer
+pnpm i @metaplex-foundation/umi-bundle-tests
+pnpm add @metaplex-foundation/mpl-token-metadata
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, TypeScript!');
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import { generateSigner, signerIdentity } from '@metaplex-foundation/umi'
+
+const umi = createUmi('https://api.devnet.solana.com')
+
+// Generate a new keypair signer.
+const signer = generateSigner(umi)
+
+// Tell Umi to use the new signer.
+umi.use(signerIdentity(signer))
+
+
+// register mpl-token-metadata to umi
+import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
+
+umi.use(mplTokenMetadata());
